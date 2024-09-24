19b9b02a130d130fa67492c7a655f5fcad905f6a
lunarifish <messenger@lunariio.work>
2024-09-24 11:07:01 +0800

    cleanup&fix

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -98,9 +98,6 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
-    HAL_Delay(1000);
-    test.a++;
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
